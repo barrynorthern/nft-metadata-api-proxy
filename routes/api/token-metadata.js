e6b4104af6706f293ed126be3dad3d32dd8aac18
Barry Northern <barry.n@spikeglobal.com>
2022-06-29 14:38:19 +0100

    allow through cors

--- routes/api/token-metadata.js
+++ routes/api/token-metadata.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var metadataRepo = require('../../helpers/metadata-repo')
 
 router.get('/', function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   metadataRepo.getAll()
     .then((totalSupply) => {
       return res.json({totalSupply: totalSupply});
@@ -10,6 +15,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:tokenId', function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   let tokenId = parseInt(req.params.tokenId);
   metadataRepo.getById(tokenId)
     .then((data) => {
