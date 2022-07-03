2dc58a92b94bc73cee604ba49be5e3a17be494ea
Barry Northern <barry.n@spikeglobal.com>
2022-07-03 18:23:08 +0100

    switch to non-checking reveal mechanism

--- helpers/metadata-repo.js
+++ helpers/metadata-repo.js
@@ -1,13 +1,13 @@
-const { ethers } = require('ethers')
-const ERC721_ABI = require('../contracts/ERC721.json')
-const CacheService = require('../cache')
+// const { ethers } = require('ethers')
+// const ERC721_ABI = require('../contracts/ERC721.json')
+// const CacheService = require('../cache')
 const fetch = require('node-fetch');
 
-const ttl = 30; //cache for 30 seconds by default, overriden to 0 (unlimited) for getById below;
-const cache = new CacheService(ttl);
+// const ttl = 30; //cache for 30 seconds by default, overriden to 0 (unlimited) for getById below;
+// const cache = new CacheService(ttl);
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_RPC_URL);
-const erc721Contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, ERC721_ABI.abi, provider);
+// const provider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_RPC_URL);
+// const erc721Contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, ERC721_ABI.abi, provider);
 
 const AUTO_REVEAL_ID = 1200;
 
@@ -22,31 +22,38 @@ function getMetadata(id) {
 }
 
 const MetadataRepo = {
-  getAll() {
-    return cache.get("TotalSupply", () => erc721Contract.totalSupply().then((bigNumber) => bigNumber.toNumber()))
-      .then((total) => {
-        return total;
-      });
-  },
+  // getAll() {
+  //   return cache.get("TotalSupply", () => erc721Contract.totalSupply().then((bigNumber) => bigNumber.toNumber()))
+  //     .then((total) => {
+  //       return total;
+  //     });
+  // },
   
   getById(id) {
     if (id <= AUTO_REVEAL_ID) {
       return getMetadata(id);
     }
-
-    return cache.get(`Token_${id}`, () => {
-        return erc721Contract
-        .ownerOf(id)
-        .then(() => true)
-        .catch(() => false);
-      }, 0)
-      .then((exists) => {
-        if (exists) {
-          return getMetadata(id);
-        } else {
-          return { error: `You may lookee for number ${id} but yees gotta mint it first. SNEAK!`};
-        }
-      });
+    else {
+      return new Promise({
+        image: "https://ipfs.io/ipfs/bafkreievyqdorbs66dm4cbno3oogafklpyodncjmnxx5smkeojbequpxgm",
+        name: "Goblin Loot WTF - Unrevealed",
+        description: "What the feck? Iz mostly CRUD! May-b summink speshul. May-b.",
+        attributes: []
+        });
+    }
+    // return cache.get(`Token_${id}`, () => {
+    //     return erc721Contract
+    //     .ownerOf(id)
+    //     .then(() => true)
+    //     .catch(() => false);
+    //   }, 0)
+    //   .then((exists) => {
+    //     if (exists) {
+    //       return getMetadata(id);
+    //     } else {
+    //       return { error: `You may lookee for number ${id} but yees gotta mint it first. SNEAK!`};
+    //     }
+    //   });
   }
 };
 
--- routes/api/token-metadata.js
+++ routes/api/token-metadata.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 var metadataRepo = require('../../helpers/metadata-repo')
 
-router.get('/', function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  metadataRepo.getAll()
-    .then((totalSupply) => {
-      return res.json({totalSupply: totalSupply});
-    })
-});
+// router.get('/', function(req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   metadataRepo.getAll()
+//     .then((totalSupply) => {
+//       return res.json({totalSupply: totalSupply});
+//     })
+// });
 
 router.get('/:tokenId', function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
